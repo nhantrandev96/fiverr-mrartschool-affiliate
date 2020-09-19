3b84b11bd55b3f7bf3c3d93b8563fa8c1387bb4c
Nhan Tran <tranvannhan100896@gmail.com>
2020-09-19 13:36:13 +0700

    Set permission

---
+++ application/logs/log-2020-09-19.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-09-19 11:49:49 --> The upload destination folder does not appear to be writable.
+ERROR - 2020-09-19 11:49:58 --> The upload destination folder does not appear to be writable.
+ERROR - 2020-09-19 11:50:16 --> The upload destination folder does not appear to be writable.
+ERROR - 2020-09-19 11:50:29 --> The upload destination folder does not appear to be writable.
+ERROR - 2020-09-19 11:53:14 --> The upload destination folder does not appear to be writable.
+ERROR - 2020-09-19 11:53:37 --> Unable to save the image. Please make sure the image and file directory are writable.
+ERROR - 2020-09-19 11:55:53 --> 404 Page Not Found: 
+ERROR - 2020-09-19 11:56:29 --> 404 Page Not Found: 
